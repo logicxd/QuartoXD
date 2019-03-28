5fdb2b6cc7224f12e967f1227148854ef0d016c9
Aung David Moe <aungmoe12@yahoo.com>
2019-03-27 22:42:32 -0700

    Code clean up

--- app/QuartoXD/Base/CircularTransition.m
+++ app/QuartoXD/Base/CircularTransition.m
@@ -28,33 +28,32 @@
   }
   
   UIView *containerView = transitionContext.containerView;
-  
-  UIView *backgroundView = [self backgroundViewOfVC:toVC];
+  UIView *transitionView = [self transitionView:toVC];
   
   [containerView addSubview:snapshot];
+  [containerView addSubview:transitionView];
   [fromVC.mainView removeFromSuperview];
-  [containerView addSubview:backgroundView];
-  [self circularExpand:backgroundView vc:toVC];
+
+  [self circularExpand:transitionView size:toVC.view.frame.size];
 }
 
-- (UIView *)backgroundViewOfVC:(BaseViewController *)toVC {
-  UIView *backgroundView = [[UIView alloc] init];
-  backgroundView.frame = CGRectZero;
-  backgroundView.center = toVC.mainView.center;
-  backgroundView.backgroundColor = toVC.mainView.backgroundColor;
-  return backgroundView;
+- (UIView *)transitionView:(BaseViewController *)vc {
+  UIView *view = [[UIView alloc] init];
+  view.frame = CGRectZero;
+  view.center = vc.mainView.center;
+  view.backgroundColor = vc.mainView.backgroundColor;
+  return view;
 }
 
-- (void)circularExpand:(UIView *)view vc:(BaseViewController *)vc {
+- (void)circularExpand:(UIView *)view size:(CGSize)size {
   CGFloat initialSize = 10;
   [view setSize:CGSizeMake(initialSize, initialSize)];
   view.layer.cornerRadius = initialSize/2;
 
-  [UIView animateWithDuration:0.75 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
-    CGSize vcSize = vc.view.frame.size;
-    CGFloat fullSize = vcSize.width > vcSize.height ? vcSize.width : vcSize.height;
+  [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveEaseIn animations:^{
+    CGFloat fullSize = size.width > size.height ? size.width : size.height;
     CGFloat scale = fullSize / initialSize;
-    scale = scale * 1.3f;  // Screen corner doesn't get covered without this
+    scale = scale * 1.15f;  // Cover the corners
     
     view.transform = CGAffineTransformMakeScale(scale, scale);
   } completion:nil];
--- app/QuartoXD/Menu/MenuViewController.m
+++ app/QuartoXD/Menu/MenuViewController.m
@@ -29,7 +29,7 @@
 #pragma mark - Button Delegates
 
 - (void)buttonPressedSinglePlayer {
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
     dispatch_async(dispatch_get_main_queue(), ^{
       QuartoBoardViewController *vc = [[QuartoBoardViewController alloc] init];
       [self.navigationController pushViewController:vc animated:YES];
